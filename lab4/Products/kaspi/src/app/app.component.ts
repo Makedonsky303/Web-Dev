2a5f1d9dee836c977788f74b6835ea1069e915df
admin <iskanderzhanazarov.2@gmail.com>
2025-02-16 21:47:32 +0500

    4_lab_4_commit

--- lab4/Products/kaspi/src/app/app.component.ts
+++ lab4/Products/kaspi/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [CommonModule,RouterOutlet,HomeComponent],
+  imports: [CommonModule,RouterOutlet,HomeComponent,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- lab4/Products/kaspi/src/app/home/home.component.ts
+++ lab4/Products/kaspi/src/app/home/home.component.ts
@@ -13,20 +13,79 @@ import { ProductInfo } from '../product-info';
 export class HomeComponent {
     productList: ProductInfo[] = [
 
+      
       {
         "photo": "megaminx_1.jpg",
-        "name": "Megaminx",
-        "description": "Made of plastic",
+        "name": "Мегаминск",
+        "description": "Сделан из пластика",
         "rating": 5,
         "link": "https://kaspi.kz/shop/p/kubik-rubika-zerde-toys-plastik-120867052/?c=750000000"
       },
       {
-        "photo": "photo",
-        "name": "name",
-        "description": "desc",
+        "photo": "QiYi5by5.jpg",
+        "name": "Кубик Рубика QiYi MoFangGe WuShuang 5x5",
+        "description": "Мультиколор, без магнитов",
         "rating": 5,
-        "link": "link"
+        "link": "https://kaspi.kz/shop/p/kubik-rubika-golovolomka-qiyi-mofangge-wushuang-5x5-47011-2-mul-tikolor-bez-magnitov-112908453/?c=750000000"
+      },
+      {
+        "photo": "GAN_Speedcube.jpg",
+        "name": "Кубик Рубика GAN Speedcube",
+        "description": "Пластиковый кубик Рубика размером 5.6x5.6x5.6 см",
+        "rating": 5,
+        "link": "https://kaspi.kz/shop/p/kubik-rubika-gan-speedcube-plastik-112153487/"
+      },
+      {
+        "photo": "GAN_356M.jpg",
+        "name": "Кубик Рубика GAN 356M магнитный",
+        "description": "Магнитный кубик Рубика 5.6x5.6x5.6 см",
+        "rating": 5,
+        "link": "https://kaspi.kz/shop/p/kubik-rubika-gan-356m-magnitnyi-109783051/"
+      },
+      {
+        "photo": "QiYi_2x2.jpg",
+        "name": "Головоломка QiYi Toys Кубик Рубика 2x2 QiDi (S)",
+        "description": "Пластиковый кубик 5х5х5 см, подходит для всех",
+        "rating": 5,
+        "link": "https://kaspi.kz/shop/p/golovolomka-qiyi-toys-kubik-rubika-2h2-qidi-s-plastik-100068556/"
+      },
+      {
+        "photo": "QiYi_3x3.jpeg",
+        "name": "Кубик Рубика QiYi Toys головоломка пластик",
+        "description": "Пластиковый кубик 5.6х5.6х5.6 см",
+        "rating": 5,
+        "link": "https://kaspi.kz/shop/p/kubik-rubika-qiyi-toys-golovolomka-plastik-117038999/"
+      },
+      {
+        "photo": "GAN_356M_Lite.jpg",
+        "name": "Кубик Рубика GAN 356M lite",
+        "description": "Пластиковый кубик 5.6x5.6x5.6 см",
+        "rating": 5,
+        "link": "https://kaspi.kz/shop/p/gan-kubik-rubika-3x3-356m-lite-108060916/"
+      },
+      {
+        "photo": "GAN_356XS.jpg",
+        "name": "Магнитный Кубик Рубика 3x3 GAN356 XS Magnetic",
+        "description": "Магнитный кубик 10х15 см, пластик и металл",
+        "rating": 5,
+        "link": "https://kaspi.kz/shop/p/gan-magnitnyi-kubik-rubika-3x3-gan356-xs-magnetic-108482609/"
+      },
+      {
+        "photo": "QiYi_White.jpg",
+        "name": "Кубик Рубика QiYi Toys головоломка пластик",
+        "description": "Белый-оранжевый кубик 6х3 см",
+        "rating": 5,
+        "link": "https://kaspi.kz/shop/p/kubik-rubika-qiyi-toys-golovolomka-plastik-112816782/"
+      },
+      {
+        "photo": "GAN_356M_Multicolor.jpg",
+        "name": "Кубик Рубика GAN 356 M мультиколор",
+        "description": "Пластиковый кубик 5.6x5.6x5.6 см",
+        "rating": 5,
+        "link": "https://kaspi.kz/shop/p/gan-kubik-rubika-3x3-356-m-mul-tikolor-108060858/"
       }
+      
+      
 
     ];
 }
